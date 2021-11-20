773fa8277d81949272533011854de2c700c4a1e0
Jami Kettunen <jami.kettunen@hotmail.com>
2021-11-20 22:48:10 +0200

    ASoC: qcom: lpass-msm8998: Add platform driver for lpass audio
    Add platform driver for configuring msm8998 lpass core I2S and
    DMA configuration to support playback & capture to external codecs
    connected over primary & secondary MI2S interfaces.
    
    (driver originally split from 4d7e1ead3b08 ("fastmatrix yaml fix"))

---
+++ include/dt-bindings/sound/msm8998-lpass.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __DT_MSM8998_LPASS_H
+#define __DT_MSM8998_LPASS_H
+
+#include <dt-bindings/sound/qcom,lpass.h>
+
+/* NOTE: Use qcom,lpass.h to define any AIF ID's for LPASS */
+
+#endif /* __DT_MSM8998_LPASS_H */
